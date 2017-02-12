6ed9c32659c1fba753124682b8e5ece00f9375b5
Andry X. Pedak <andry.pedak@gmail.com>
2017-02-12 23:04:37 +0200

    View template

---
+++ view.php
@@ -0,0 +1,45 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+	<div class="bar"><a href="#"><i class="fa fa-angle-double-left" aria-hidden="true"></i> Tagasi otsingusse</a></div>
+
+	<div class="row">
+		<div class="col s12 m5">
+			<img class="materialboxed" src="img/21K-Fo-2.1-2.jpg" width="100%">
+		</div>
+		<div class="col s12 m7">
+			<div class="details">
+				<div class="peace">
+					Foto tegemise aeg<br>
+					<span>1930</span>
+				</div>
+				<div class="peace">
+					Kes/mis on fotol<br>
+					<span>Koolijuhataja (direktor) Enn Murdmaa (Ernst Martinson) portree. Koolijuhataja (direktor) Enn Murdmaa (Ernst Martinson) portree.</span>
+				</div>
+				<div class="peace">
+					Kus foto tehtud on<br>
+					<span>&mdash;</span>
+				</div>
+				<div class="peace">
+					Fotograaf<br>
+					<span>&mdash;</span>
+				</div>
+				<div class="peace">
+					Foto mõõtmed<br>
+					<span>&mdash;</span>
+				</div>
+
+				<a href="#"><div class="chip">Koolijuhataja</div></a>
+				<a href="#"><div class="chip">Direktor</div></a>
+				<a href="#"><div class="chip">1980ndad</div></a>
+				<a href="#"><div class="chip">21k</div></a>
+				<a href="#"><div class="chip">Enn Murdmaa</div></a>
+				<a href="#"><div class="chip">Portree</div></a>
+				<a href="#"><div class="chip">1980</div></a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.php'; ?>
