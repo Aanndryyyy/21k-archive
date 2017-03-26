8ca62264e1eec8dd904141256ec425134744cc44
Andry X. Pedak <andry.pedak@gmail.com>
2017-03-26 22:36:39 +0300

    Build

---
+++ build/arhiivist.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+
+<!-- Search -->
+<div class="container">
+  <div class="page-headr">
+    <h1 class="center-align">ARHIIVIST</h1>
+  </div>
+
+  <div class="row">
+    <div class="col m6 s12">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col m6 s12">
+      <img class="responsive-img" src="http://21k.ee/wp-content/uploads/2010/08/Tallinna-21.-Kool.jpg">
+    </div>
+  </div>
+</div>
+
+<?php include 'footer.php'; ?>
---
+++ build/contact.php
@@ -0,0 +1,32 @@
+<?php include 'header.php'; ?>
+
+<!-- Search -->
+<div class="container">
+  <div class="page-headr">
+    <h1 class="center-align">KONTAKT</h1>
+  </div>
+
+  <div class="row">
+  	<div class="col m4 offset-m2 s12">
+  		<div class="mail">
+  			<p><i class="fa fa-envelope-o" aria-hidden="true"></i> arhiiv@21k.ee</p>
+  		</div>
+  	</div>
+  	<div class="col m4 s12">
+			<div class="phone">
+				<p><i class="fa fa-phone" aria-hidden="true"></i> +372 593 3235</p>
+			</div>
+  	</div>
+  </div>
+
+  <div class="row">
+  	<div class="col m6 offset-m3 s12">
+  		<p class="contact-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  		consequat.</p>
+  	</div>
+  </div>
+</div>
+
+<?php include 'footer.php'; ?>
