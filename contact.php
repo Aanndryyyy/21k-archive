5f55b7b07cb4cd6d96ec4d9dd740caad0acf5934
Andry X. Pedak <andry.pedak@gmail.com>
2017-03-26 21:19:49 +0300

    Added contact page

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php include 'header.php'; ?>
+
+<!-- Search -->
+<div class="container">
+  <div class="otsing">
+    <h1 class="center-align">Kontakt</h1>
+  </div>
+
+	<div class="row">
+		
+	</div>
+</div>
+
+<?php include 'footer.php'; ?>
